d8692301042e6ee71fdb58389f9daeac8c76c61d
nick880107 <nick701088@gmail.com>
2020-10-18 12:15:53 +0800

    Update Dynamic Array

--- hw2_maze/maze_bfs(recursive_ver).cpp
+++ hw2_maze/maze_bfs(recursive_ver).cpp
@@ -1,9 +1,8 @@
 #include <cstdio>
-#include <cstring>
 #include <iostream>
 #include <queue>
-#define MAXSTACK 400
-#define MAXMAP 20 
+#define MAXSTACK 100
+#define NEW2D(H, W, TYPE) (TYPE **)new2d(H, W, sizeof(TYPE))
 using namespace std;
 
 struct point{
@@ -12,33 +11,41 @@ struct point{
 	point *last;
 };
 
-int m,n;
-int map[MAXMAP][MAXMAP], vis[MAXMAP][MAXMAP];
-int top=-1,path[MAXSTACK][2];
+
+int top=-1;
 int found=0;
 queue <point*> q;
 int dir[8][2]={{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}};
-point *start=new point;
-point target;
+void bfs(int **data,int **vis,point *start,point target,int **path);
+void print(int **path);
+void *new2d(int m,int n,int size);
 
 
-void bfs();
-void print();
 
 
 int main(){
-	
+	int m,n;
 	printf("請輸入迷宮大小：");
 	scanf("%d %d",&m,&n);
 	
 	//初始化陣列 
+	int **data=NEW2D(m+2,n+2,int);
+	int **vis=NEW2D(m+2,n+2,int);
+	int **path=NEW2D(m*n,2,int);
+	
 	for(int t=0;t<n+2;t++){
-		map[0][t]=1;
-		map[m+1][t]=1;
+		data[0][t]=1;
+		data[m+1][t]=1;
 	} 
 	for(int t=0;t<m+2;t++){
-		map[t][0]=1;
-		map[t][n+1]=1;
+		data[t][0]=1;
+		data[t][n+1]=1;
+	}
+	
+	for(int i=0;i<m+2;i++){
+		for(int j=0;j<n+2;j++){
+			vis[i][j]=0;
+		}
 	}
 	
 	printf("請輸入迷宮資料：\n");
@@ -46,20 +53,43 @@ int main(){
 		int j=1;
 		char y;
 		do{
-			scanf("%d",&map[i][j]);
+			scanf("%d",&data[i][j]);
 			j++;
-		}while(y=getchar()!='\n');
+		}while((y=getchar())!='\n');
 	}
-	target.row=m;
-	target.col=n;
+	
+	//設定起點終點
+	point *start=new point;
 	start->row=1;
 	start->col=1;
-	vis[start->row][start->col]=1;
+	point target;	 
+	target.row=m;
+	target.col=n;
+
+	vis[1][1]=1;
 	q.push(start);
-	bfs();
-	print();
+	bfs(data,vis,start,target,path);
+	print(path);
 }
-void bfs(){
+
+void* new2d(int h, int w, int size)
+{
+    int i;
+	void **p;
+	try{
+		p = (void**)new char[h*sizeof(void*) + h*w*size];
+	}
+   	catch(std::bad_alloc){
+   		printf("out of memory");
+	}
+    
+    for(i = 0; i < h; i++)
+        p[i] = ((char *)(p + h)) + i*w*size;
+
+    return p;
+}
+
+void bfs(int **data,int **vis,point *start,point target, int **path){
 	while(!q.empty()){
 		point *position=q.front(); //取得queue儲存的第一個位置
 		q.pop();
@@ -81,7 +111,7 @@ void bfs(){
 				int row,col;
 				row=position->row+dir[i][0];
 				col=position->col+dir[i][1];
-				if(map[row][col]==0 && vis[row][col]==0){
+				if(data[row][col]==0 && vis[row][col]==0){
 					point* temp=new point;
 					temp->row=row;
 					temp->col=col;
@@ -94,16 +124,23 @@ void bfs(){
 	}
 }
 
-void print(){
+void print(int **path){
 	if(found==0){
 		printf("不存在路徑！");
 	}
-	else if(top>-1){
-		printf("%d %d\n",path[top][0],path[top][1]);
-		top--;
-		print();
-	}
 	else{
-		return;
+		while(top>-1){
+			printf("%d %d\n",path[top][0],path[top][1]);
+			top--;	
+		}
+
 	}
 }
+
+	
+//	for(int i=0;i<m+2;i++){
+//		for(int j=0;j<n+2;j++){
+//			printf("%d ",vis[i][j]);
+//		}
+//		printf("\n");
+//	}
